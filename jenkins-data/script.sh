b602b4bd24df7fddfb4a4fe904209e2563be3352
kkruel55 <k.kruelskie@gmail.com>
2021-05-20 09:20:14 -0600

    docker remote ssh server set up

--- jenkins-data/script.sh
+++ jenkins-data/script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+NAME=$1
+LASTNAME=$2
+SHOW=$3
 
-echo "Hello $NAME $LASTNAME"
-
+if [ "$SHOW" = "true" ]; then
+  echo "Hello $NAME $LASTNAME"
+else
+  echo "If you want to see the name, please mark the show option. "
+fi
